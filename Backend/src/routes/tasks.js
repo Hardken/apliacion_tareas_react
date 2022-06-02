d11db7c4c81949f29f2ab478ef2d0b0f108e15c5
Andres Vega <anfevo01@gmail.com>
2022-06-02 03:42:08 -0500

    Completando documentacion

--- Backend/src/routes/tasks.js
+++ Backend/src/routes/tasks.js
@@ -8,52 +8,66 @@ import {
   updateTasks,
 } from "../controllers/tasks";
 const router = Router();
-
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks endpoint
+ */
 /**
  * @swagger
  * /tasks:
- * get:
- *  summary: Get all the tasks
+ *  get:
+ *    summary: Get all the tasks
+ *    tags: [Tasks]
  *
  */
 router.get("/tasks", getTasks);
 /**
  * @swagger
  * /tasks/count:
- * get:
- *  summary: Get total tasks counter
+ *  get:
+ *    summary: Get total tasks counter
+ *    tags: [Tasks]
+ * 
  *
  */
 router.get("/tasks/count", getTaskCount);
 /**
  * @swagger
  * /tasks:
- * get:
- *  summary: Get a tasks by id
+ *  get:
+ *    summary: Get a tasks by id
+ *    tags: [Tasks]
+ * 
  *
  */
 router.get("/tasks/:id", getTask);
 /**
  * @swagger
  * /tasks:
- * post:
- *  summary: Save a new tasks
+ *  post:
+ *    summary: Save a new tasks
+ *    tags: [Tasks]
+ * 
  *
  */
 router.post("/tasks", saveTasks);
 /**
  * @swagger
  * /tasks:
- * delete:
- *  summary: delete a tasks by id
+ *  delete:
+ *    summary: delete a tasks by id
+ *    tags: [Tasks]
  *
  */
 router.delete("/tasks/:id", deleteTasks);
 /**
  * @swagger
  * /tasks:
- * put:
- *  summary: update a tasks by id
+ *  put:
+ *   summary: update a tasks by id
+ *   tags: [Tasks]
  *
  */
 router.put("/tasks/:id", updateTasks);
